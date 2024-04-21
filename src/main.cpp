f25be6d5ec0eae60e11a11b38cea863b3086c1a0
Eren KÜRPE <84393333+kurpeeren@users.noreply.github.com>
2024-04-21 16:55:25 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,124 @@
-#include <Arduino.h>
-#include <ArduinoRS485.h>
-#include <SoftwareSerial.h>
-#include <ArduinoModbus.h>
-#include <Ethernet.h>
+#include <EtherCard.h> //EtherCard kütüphanesi ekleme
+
+//Ledi yakıp söndürmek için kısayollar
+//http://192.168.2.34/?LED=OFF
+//http://192.168.2.34/?LED=ON
 
 
+#define STATIC 0 // DHCP'yi kullanım dışı bırakmak için 1 yapılır (myip/gwip değerlerini aşağıda ayarlanır)
 
-// Ethernet ayarları
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Ethernet kartınızın MAC adresi
-IPAddress ip(192, 168, 1, 100); // Arduino'nun IP adresi
-IPAddress gateway(192, 168, 1, 1); // Ağ geçidi
-IPAddress subnet(255, 255, 255, 0); // Alt ağ maskesi
+// mac adresi
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+// ethernet ip adresi
+static byte myip[] = { 192,168,0,200 };
+// gateway ip adresi
+static byte gwip[] = { 192,168,0,1 };
 
-// RS485 ayarları
-#define RS485TxEnablePin 2 // RS485 için TxEnable pini
-#define RS485Serial Serial1 // RS485 için kullanılacak seri port
+// Kontrol edilecek LED pini
+int ledPin = 5; //Arduino D5 pini ile GND arasına LED ve 150ohm direnç bağlanır.
 
-// Modbus ayarları
-ModbusIP mb;
-const uint16_t regCount = 10; // Okunacak register sayısı
-uint16_t holdingRegs[regCount]; // Okunan register değerlerini saklamak için dizi
+byte Ethernet::buffer[700];
 
-void setup() {
-  Serial.begin(9600);
-  RS485Serial.begin(9600);
-  
-  // Ethernet başlatma
-  Ethernet.begin(mac, ip, gateway, gateway, subnet);
-  mb.server(); // Modbus sunucu olarak başlatma
+//web arayüzü
+char const page[] PROGMEM =
+"HTTP/1.0 503 Service Unavailable\r\n"
+"Content-Type: text/html\r\n"
+"Retry-After: 600\r\n"
+"\r\n"
+"<html>"
+"<head><title>"
+"Service Temporarily Unavailable"
+"</title></head>"
+"<body>"
+"<h3>LED KONTROL PROGRAMI</h3>"
+"<p><em>"
+"LED kontrolu icin Arduino'ya iletilecek komutlar.<br />"
+"Sintaks yapisi: http://192.168.0.XX/?LED=OFF or ON"
+"</em></p>"
+"</body>"
+"</html>"
+;
 
-  // RS485 ayarları
-  pinMode(RS485TxEnablePin, OUTPUT);
-  digitalWrite(RS485TxEnablePin, LOW); // RS485 modunda dinleme moduna geçme
+void setup () 
+{
+    pinMode(ledPin, OUTPUT);
+    
+    Serial.begin(9600);
+    Serial.println("Trying to get an IP...");
+    
+    Serial.print("MAC: ");
+    for (byte i = 0; i < 6; ++i) 
+    {
+        Serial.print(mymac[i], HEX);
+        if (i < 5)
+        Serial.print(':');
+    }
+    Serial.println();
+    
+    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
+    {
+        Serial.println( "Failed to access Ethernet controller");
+    }
+    else
+    {
+        Serial.println("Ethernet controller access: OK");
+    }
+;
 
-  Serial.println("Setup completed.");
+#if STATIC
+    Serial.println( "Getting static IP.");
+    if (!ether.staticSetup(myip, gwip))
+    {
+      Serial.println( "could not get a static IP");
+      blinkLed(); // blink forever to indicate a problem
+    }
+#else
+
+    Serial.println("Setting up DHCP");
+    if (!ether.dhcpSetup())
+    {
+        Serial.println( "DHCP failed");
+        blinkLed(); // blink forever to indicate a problem
+    }
+#endif
+
+    ether.printIp("My IP: ", ether.myip);
+    ether.printIp("Netmask: ", ether.netmask);
+    ether.printIp("GW IP: ", ether.gwip);
+    ether.printIp("DNS IP: ", ether.dnsip);
 }
 
-void loop() {
-  mb.task();
+void loop () 
+{
+    word len = ether.packetReceive();
+    word pos = ether.packetLoop(len);
 
-  // RS485 üzerinden veri okuma
-  digitalWrite(RS485TxEnablePin, HIGH); // RS485 modunda veri okuma
-  if (RS485Serial.available() >= 4) {
-    uint8_t buffer[4];
-    RS485Serial.readBytes(buffer, 4);
-    holdingRegs[0] = word(buffer[0], buffer[1]); // Örnek olarak ilk 2 baytı bir register'a atama
-    holdingRegs[1] = word(buffer[2], buffer[3]); // Sonraki 2 baytı başka bir register'a atama
-  }
-  digitalWrite(RS485TxEnablePin, LOW); // RS485 modunda dinleme moduna geçme
+    // IF LED=ON turn it ON
+    if(strstr((char *)Ethernet::buffer + pos, "GET /?LED=ON") != 0) 
+    {
+        Serial.println("Received ON command");
+        digitalWrite(ledPin, HIGH);
+    }
+    
+    // IF LED=OFF turn it OFF
+    if(strstr((char *)Ethernet::buffer + pos, "GET /?LED=OFF") != 0) 
+    {
+        Serial.println("Received OFF command");
+        digitalWrite(ledPin, LOW);
+    }
+    
+    // show some data to the user
+    memcpy_P(ether.tcpOffset(), page, sizeof page);
+    ether.httpServerReply(sizeof page - 1);
+}
 
-  // Modbus üzerinden okunan verileri yayınlama
-  mb.Hreg(0, holdingRegs[0]);
-  mb.Hreg(1, holdingRegs[1]);
+void blinkLed()
+{
+    while (true)
+    {
+        digitalWrite(ledPin, HIGH);
+        delay(500);
+        digitalWrite(ledPin, LOW);
+        delay(500);
+    }
 }
