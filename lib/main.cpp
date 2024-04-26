24b97f519981a8c47ad3f1ed5c88e6247272e472
Eren KÜRPE <84393333+kurpeeren@users.noreply.github.com>
2024-04-26 16:20:17 +0300

    Update

---
+++ lib/main.cpp
@@ -0,0 +1,131 @@
+#include <EtherCard.h> //EtherCard kütüphanesi ekleme
+#include <SPI.h>
+#include <ArduinoRS485.h>
+
+//Ledi yakıp söndürmek için kısayollar
+//http://192.168.2.34/?LED=OFF
+//http://192.168.2.34/?LED=ON
+
+
+#define STATIC 1 // DHCP'yi kullanım dışı bırakmak için 1 yapılır (myip/gwip değerlerini aşağıda ayarlanır)
+
+void blinkLed();
+
+// mac adresi
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+// ethernet ip adresi
+static byte myip[] = { 192,168,0,200 };
+// gateway ip adresi
+static byte gwip[] = { 192,168,0,254 };
+
+// Kontrol edilecek LED pini
+int ledPin = 5; //Arduino D5 pini ile GND arasına LED ve 150ohm direnç bağlanır.
+
+byte Ethernet::buffer[700];
+
+//web arayüzü
+char const page[] PROGMEM =
+"HTTP/1.0 503 Service Unavailable\r\n"
+"Content-Type: text/html\r\n"
+"Retry-After: 600\r\n"
+"\r\n"
+"<html>"
+"<head><title>"
+"Service Temporarily Unavailable"
+"</title></head>"
+"<body>"
+"<h3>LED KONTROL PROGRAMI</h3>"
+"<p><em>"
+"LED kontrolu icin Arduino'ya iletilecek komutlar.<br />"
+"Sintaks yapisi: http://192.168.0.XX/?LED=OFF or ON"
+"</em></p>"
+"</body>"
+"</html>"
+;
+
+void blinkLed()
+{
+    while (true)
+    {
+        digitalWrite(ledPin, HIGH);
+        delay(500);
+        digitalWrite(ledPin, LOW);
+        delay(500);
+    }
+}
+void setup () 
+{
+    pinMode(ledPin, OUTPUT);
+    // Haberleşme Ayarları
+    Serial.begin(9600);
+    Serial.println("Trying to get an IP...");
+    
+    Serial.print("MAC: ");
+    for (byte i = 0; i < 6; ++i) 
+    {
+        Serial.print(mymac[i], HEX);
+        if (i < 5)
+        Serial.print(':');
+    }
+    Serial.println();
+    
+    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
+    {
+        Serial.println( "Failed to access Ethernet controller");
+    }
+    else
+    {
+        Serial.println("Ethernet controller access: OK");
+    };
+
+//DHCP Ayarı
+#if STATIC
+    Serial.println( "Getting static IP.");
+    if (!ether.staticSetup(myip, gwip))
+    {
+      Serial.println( "could not get a static IP");
+      blinkLed(); // blink forever to indicate a problem
+    }
+#else
+
+    Serial.println("Setting up DHCP");
+    if (!ether.dhcpSetup())
+    {
+        Serial.println( "DHCP failed");
+        blinkLed(); // blink forever to indicate a problem
+    }
+#endif
+    ether.printIp("My IP: ", ether.myip);
+    ether.printIp("Netmask: ", ether.netmask);
+    ether.printIp("GW IP: ", ether.gwip);
+    ether.printIp("DNS IP: ", ether.dnsip);
+}
+
+
+void loop () 
+{
+    word len = ether.packetReceive();
+    word pos = ether.packetLoop(len);
+
+    // IF LED=ON turn it ON
+    if(strstr((char *)Ethernet::buffer + pos, "GET /?LED=ON") != 0) 
+    {
+        Serial.println("Received ON command");
+        digitalWrite(ledPin, HIGH);
+    }
+    
+    // IF LED=OFF turn it OFF
+    if(strstr((char *)Ethernet::buffer + pos, "GET /?LED=OFF") != 0) 
+    {
+        Serial.println("Received OFF command");
+        digitalWrite(ledPin, LOW);
+    }
+    
+    // show some data to the user
+    memcpy_P(ether.tcpOffset(), page, sizeof page);
+    ether.httpServerReply(sizeof page - 1);
+}
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,131 +1,50 @@
-#include <EtherCard.h> //EtherCard kütüphanesi ekleme
 #include <SPI.h>
+#include <Ethernet.h>
 #include <ArduinoRS485.h>
 
-//Ledi yakıp söndürmek için kısayollar
-//http://192.168.2.34/?LED=OFF
-//http://192.168.2.34/?LED=ON
+// MAC ve IP adresleri
+byte mac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };
+IPAddress ip(192, 168, 0, 200);
+EthernetServer server(1234);  // Düz soket programlama için farklı bir port (örneğin 1234)
 
+int ledPin = 5;
 
-#define STATIC 0 // DHCP'yi kullanım dışı bırakmak için 1 yapılır (myip/gwip değerlerini aşağıda ayarlanır)
-
-void blinkLed();
-
-// mac adresi
-static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
-// ethernet ip adresi
-static byte myip[] = { 192,168,0,200 };
-// gateway ip adresi
-static byte gwip[] = { 192,168,0,1 };
-
-// Kontrol edilecek LED pini
-int ledPin = 5; //Arduino D5 pini ile GND arasına LED ve 150ohm direnç bağlanır.
-
-byte Ethernet::buffer[700];
-
-//web arayüzü
-char const page[] PROGMEM =
-"HTTP/1.0 503 Service Unavailable\r\n"
-"Content-Type: text/html\r\n"
-"Retry-After: 600\r\n"
-"\r\n"
-"<html>"
-"<head><title>"
-"Service Temporarily Unavailable"
-"</title></head>"
-"<body>"
-"<h3>LED KONTROL PROGRAMI</h3>"
-"<p><em>"
-"LED kontrolu icin Arduino'ya iletilecek komutlar.<br />"
-"Sintaks yapisi: http://192.168.0.XX/?LED=OFF or ON"
-"</em></p>"
-"</body>"
-"</html>"
-;
-
-void blinkLed()
-{
-    while (true)
-    {
-        digitalWrite(ledPin, HIGH);
-        delay(500);
-        digitalWrite(ledPin, LOW);
-        delay(500);
-    }
-}
-void setup () 
-{
-    pinMode(ledPin, OUTPUT);
-    
+void setup() {
     Serial.begin(9600);
-    Serial.println("Trying to get an IP...");
-    
-    Serial.print("MAC: ");
-    for (byte i = 0; i < 6; ++i) 
-    {
-        Serial.print(mymac[i], HEX);
-        if (i < 5)
-        Serial.print(':');
-    }
-    Serial.println();
-    
-    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
-    {
-        Serial.println( "Failed to access Ethernet controller");
-    }
-    else
-    {
-        Serial.println("Ethernet controller access: OK");
-    }
-;
+    pinMode(ledPin, OUTPUT);
 
-#if STATIC
-    Serial.println( "Getting static IP.");
-    if (!ether.staticSetup(myip, gwip))
-    {
-      Serial.println( "could not get a static IP");
-      blinkLed(); // blink forever to indicate a problem
-    }
-#else
+    Ethernet.begin(mac, ip);
+    server.begin();
 
-    Serial.println("Setting up DHCP");
-    if (!ether.dhcpSetup())
-    {
-        Serial.println( "DHCP failed");
-        blinkLed(); // blink forever to indicate a problem
-    }
-#endif
-
-    ether.printIp("My IP: ", ether.myip);
-    ether.printIp("Netmask: ", ether.netmask);
-    ether.printIp("GW IP: ", ether.gwip);
-    ether.printIp("DNS IP: ", ether.dnsip);
+    Serial.print("Server IP address: ");
+    Serial.println(Ethernet.localIP());
 }
 
-void loop () 
-{
-    word len = ether.packetReceive();
-    word pos = ether.packetLoop(len);
+void loop() {
+    EthernetClient client = server.available();
+    if (client) {
+        Serial.println("New client connected");
 
-    // IF LED=ON turn it ON
-    if(strstr((char *)Ethernet::buffer + pos, "GET /?LED=ON") != 0) 
-    {
-        Serial.println("Received ON command");
-        digitalWrite(ledPin, HIGH);
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                Serial.write(c);
+
+                // Gelen veriye göre LED kontrolü
+                if (c == '1') {
+                    Serial.println("Received ON command");
+                    digitalWrite(ledPin, HIGH);
+                    client.println("selam");
+                } else if (c == '0') {
+                    Serial.println("Received OFF command");
+                    digitalWrite(ledPin, LOW);
+                }
+
+                client.stop();
+                Serial.println("Client disconnected");
+
+                
+            }
+        }
     }
-    
-    // IF LED=OFF turn it OFF
-    if(strstr((char *)Ethernet::buffer + pos, "GET /?LED=OFF") != 0) 
-    {
-        Serial.println("Received OFF command");
-        digitalWrite(ledPin, LOW);
-    }
-    
-    // show some data to the user
-    memcpy_P(ether.tcpOffset(), page, sizeof page);
-    ether.httpServerReply(sizeof page - 1);
 }
-
-
-
-
